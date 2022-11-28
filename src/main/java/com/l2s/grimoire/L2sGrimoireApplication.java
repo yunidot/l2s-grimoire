5797650a29ccaf865c91c26ad8624e3c14b6d376
giyoon.kim <giyoon.kim@giyoon.kim>
2022-11-28 09:09:43 +0900

    Initial Commit

---
+++ src/main/java/com/l2s/grimoire/L2sGrimoireApplication.java
@@ -0,0 +1,13 @@
+package com.l2s.grimoire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L2sGrimoireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L2sGrimoireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/l2s/grimoire/L2sGrimoireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2s.grimoire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L2sGrimoireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
